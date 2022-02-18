67c820239a0917d189345a8082391525e49a3658
nilarkhanal97 <ddkhanal12345@gmail.com>
2022-02-18 22:46:13 +0630

    [feature/tutorial4]Tutorial4

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Tutorial4->Normalization \n");
+             Console.ReadLine();
+        }
+       
+    }
+}
