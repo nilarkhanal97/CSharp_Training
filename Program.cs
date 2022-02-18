d41d251fe565c5155fcf578a5ed8d685e897b670
nilarkhanal97 <ddkhanal12345@gmail.com>
2022-02-18 18:57:02 +0630

    [feature/tutorial2]

--- Program.cs
+++ Program.cs
@@ -6,11 +6,36 @@ using System.Threading.Tasks;
 
 namespace Tutorial2
 {
-    class Program
-    {
+   public class Program
+    {  
         static void Main(string[] args)
         {
-            Console.WriteLine("HELLO");
+            int year;
+            Console.Write("Input year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+            GetCenturyAndCheckLeapYear(year);
+            Console.ReadLine();
+        } 
+        public static void GetCenturyAndCheckLeapYear(int year)
+        {
+            int century = year / 100;
+            year %= 100;
+            int leap;
+             
+            if ((year >= 1000) || (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)))
+            {
+                leap = 1;
+                Console.WriteLine(century + "," + leap);
+            }
+            else 
+            { 
+                leap = -1;
+                Console.WriteLine(century + "," + leap);
+            }
+
+            
+
         }
+        
     }
 }
