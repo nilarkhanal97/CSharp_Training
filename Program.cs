4f27731f4e61eb85c88a481b474a6a67eb4fc79c
nilarkhanal97 <ddkhanal12345@gmail.com>
2022-02-18 22:37:00 +0630

    [feature/tutorial2]Tutorial2

--- Program.cs
+++ Program.cs
@@ -6,36 +6,35 @@ using System.Threading.Tasks;
 
 namespace Tutorial2
 {
-   public class Program
-    {  
+    class DateTimeUtility
+    {
         static void Main(string[] args)
         {
             int year;
             Console.Write("Input year : ");
             year = Convert.ToInt32(Console.ReadLine());
-            GetCenturyAndCheckLeapYear(year);
+            if(year<1000)
+                Console.WriteLine("Start year from 1000");
+            else
+                GetCenturyAndCheckLeapYear(year);
             Console.ReadLine();
         } 
         public static void GetCenturyAndCheckLeapYear(int year)
         {
             int century = year / 100;
             year %= 100;
+            century=century+1;
             int leap;
-             
-            if ((year >= 1000) || (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)))
-            {
+            if ((year>=1000) || (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)))
+            {   
                 leap = 1;
                 Console.WriteLine(century + "," + leap);
             }
-            else 
-            { 
-                leap = -1;
+            else
+            {
+                 leap = -1;
                 Console.WriteLine(century + "," + leap);
             }
-
-            
-
         }
-        
     }
 }
