fda36b0238e09ee49bab1911ce860f46ac851d2b
nilarkhanal97 <ddkhanal12345@gmail.com>
2022-02-19 11:58:44 +0630

    [feature/tutorial5]Tutorial5

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Tutorial5 \n Use db_pets \n SELECT dogs_name, cats_name from tbl_Dogs, tbs_Cats");
+             Console.ReadLine();
+        }
+    }
+}
---
+++ database/pets_sql_query.sql
@@ -0,0 +1,2 @@
+Select dogs_name, cats_name from tbl_Dogs
+full join tbl_Cats on tbl_Dogs.id= tbl_Cats.id
